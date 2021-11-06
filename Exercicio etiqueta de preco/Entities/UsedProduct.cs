3cbedbdeec4a1c96d8687dd5b11ee9899473d94e
Felipe Batista <felipe.c.b@hotmail.com>
2021-11-06 18:04:19 -0300

    exercicio completo

---
+++ Exercicio etiqueta de preco/Entities/UsedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Exercicio_etiqueta_de_preco.Entities
+{
+    class UsedProduct:Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate): base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " (used) $ " + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacure date: " + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+    }
+}
